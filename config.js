388d4a2ffe82215705050d7177370278b1dfdc2c
laotiensinh <140070109+laotiensinh@users.noreply.github.com>
2023-08-15 15:47:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-title: 'Khuê ơi, tớ là Hùng đây. bạn có thích tớ khum?? 😙',
+title: 'Babe à . bạn có thích tớ khum?? 😙',
     desc: 'chọn câu trả lời phía dưới nhé :33,
     btnYes: 'có, tớ thích bạn lắm<33',
     btnNo: 'Không, ảo tưởng à :3',
@@ -6,7 +6,7 @@ title: 'Khuê ơi, tớ là Hùng đây. bạn có thích tớ khum?? 😙',
     btnReply: 'gửi cho tớ nhé  <3',
     reply: 'Tại vì trong  tỉ người ấy thì đối với tớ bạn là đẹp nhất <33333333',
     mess: 'Tớ biết mà 🥰. tớ cũng thích bạn 😘😘',
-    messDesc: 'Khuê ơi, bạn làm người iu tớ nhé <3,
+    messDesc: ' bạn làm người iu tớ nhé <3,
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100044665072108
+    messLink: 'https://www.facebook.com
 }
